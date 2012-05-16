56110035dde9e2f6f2adb8a2c150eef6f88d16b2
Rob Mumford <rlmumford@googlemail.com>
2012-05-16 15:38:51 +0100

    Overridden on add content modal.

---
+++ templates/cns-panels-add-content-link.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * Template to control the add content individual links in the add content modal.
+ */
+?>
+<div class="cns-panels content-type-button clearfix">
+  <?php print $image_button; ?>
+  <div><?php print $text_button; ?></div>
+</div>
--- templates/cns-panels-add-content-modal.tpl.php
+++ templates/cns-panels-add-content-modal.tpl.php
@@ -4,24 +4,8 @@
  * Template to control the add content modal.
  */
 ?>
-<div class="panels-add-content-modal">
+<div class="cns-panels-add-content-modal">
+  <?php print $custom_link; ?>
+  
   <?php print $messages; ?>
-
-  <?php if (!empty($header)): ?>
-    <div class="panels-categories-description">
-      <?php print $header; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if (!empty($columns)): ?>
-  <div class="panels-section-columns">
-    <?php foreach ($columns as $column_id => $column): ?>
-      <div class="panels-section-column panels-section-column-<?php print $column_id; ?> ">
-        <div class="inside">
-          <?php print $column; ?>
-        </div>
-      </div>
-    <?php endforeach; ?>
-  </div>
-  <?php endif; ?>
 </div>
