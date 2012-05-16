abcbf6b911fb0c7b1a4af0f3008c2efce72b988b
Rob Mumford <rlmumford@googlemail.com>
2012-05-16 11:06:36 +0100

    Added renderer and pipelines stuff

--- plugins/display_renderers/panels_renderer_cns_ipe.class.php
+++ plugins/display_renderers/panels_renderer_cns_ipe.class.php
@@ -1,1 +1,1 @@
-<?php/** * Renderer class for all In-Place Editor (IPE) behavior. */class panels_renderer_cns_ipe extends panels_renderer_cns_ipe {}
+<?php/** * Renderer class for all In-Place Editor (IPE) behavior. */class panels_renderer_cns_ipe extends panels_renderer_ipe {}
